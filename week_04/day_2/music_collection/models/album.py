93c6394975cffc549efddaa30887447c41929a0d
Gergely Farkas <efgeri@gmail.com>
2023-03-21 18:34:51 +0000

    Classes created

---
+++ week_04/day_2/music_collection/models/album.py
@@ -0,0 +1,7 @@
+class Album:
+    
+    def __init__(self, title, genre, tracks, id = None):
+        self.title = title
+        self.genre = genre
+        self.tracks = tracks
+        self.id = id
