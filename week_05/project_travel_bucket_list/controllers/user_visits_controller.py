e44a961a69ef445ede369aa9d8bf350668f9a831
Gergely Farkas <efgeri@gmail.com>
2023-03-27 19:11:56 +0100

    user_visit_update route created

--- week_05/project_travel_bucket_list/controllers/user_visits_controller.py
+++ week_05/project_travel_bucket_list/controllers/user_visits_controller.py
@@ -15,44 +15,41 @@ user_visits_blueprint = Blueprint("user_visits", __name__)
 
 @user_visits_blueprint.route("/users/<id>/visits")
 def user_visits(id):
+    selected_user = user_repo.select(id)
     visits = visit_repo.select_by_user(id)
-    # pdb.set_trace()
-    return render_template("visits/index.html", all_visits = visits)
+    return render_template("user_visits/index.html", user = selected_user, all_visits = visits)
 
-@user_visits_blueprint.route("/visits/new")
-def new_visit():
-    users = user_repo.select_all()
+@user_visits_blueprint.route("/users/<id>/visits/new")
+def user_new_visit(id):
+    selected_user = user_repo.select(id)
     cities = city_repo.select_all()
-    return render_template("visits/new.html", all_users = users, all_cities = cities)
+    return render_template("user_visits/new.html", user = selected_user, all_cities = cities)
 
-@user_visits_blueprint.route("/visits", methods=['POST'])
-def create_visit():
-    user_id = request.form['user_id']
+@user_visits_blueprint.route("/users/<id>/visits", methods=['POST'])
+def user_create_visit(id):
     city_id = request.form['city_id']
-    user = user_repo.select(user_id)
+    user = user_repo.select(id)
     city = city_repo.select(city_id)
     visit = Visit(user, city)
     visit_repo.save(visit)
-    return redirect('/visits')
+    return user_visits(id)
 
 
-@user_visits_blueprint.route("/visits/<id>/edit")
-def edit_visit(id):
-    visit = visit_repo.select(id)
-    users = user_repo.select_all()
+@user_visits_blueprint.route("/users/<user_id>/visits/<visit_id>/edit")
+def edit_visit(user_id, visit_id):
+    selected_visit = visit_repo.select(visit_id)
+    selected_user = user_repo.select(user_id)
     cities = city_repo.select_all()
-    return render_template("visits/edit.html", visit = visit, all_users = users, all_cities = cities)
+    return render_template("user_visits/edit.html", visit = selected_visit, user = selected_user, all_cities = cities)
 
-@user_visits_blueprint.route("/visits/<id>", methods=['POST'])
-def update_visit(id):
-    user_id = request.form['user_id']
+@user_visits_blueprint.route("/users/<user_id>/visits/<visit_id>", methods=['POST'])
+def user_update_visit(user_id, visit_id):
     city_id = request.form['city_id']
     user = user_repo.select(user_id)
     city = city_repo.select(city_id)
-    visit = Visit(user, city, id)
-    # pdb.set_trace()
+    visit = Visit(user, city, visit_id)
     visit_repo.update(visit)
-    return redirect('/visits')
+    return user_visits(user_id)
 
 @user_visits_blueprint.route("/visits/<id>/delete", methods=['POST'])
 def delete_visit(id):
