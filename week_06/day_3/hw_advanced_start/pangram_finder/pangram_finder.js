f9443a34dca4808f2e7fac570e70ebf7823e0e60
Gergely Farkas <efgeri@gmail.com>
2023-04-05 18:45:43 +0100

    pangram done

--- week_06/day_3/hw_advanced_start/pangram_finder/pangram_finder.js
+++ week_06/day_3/hw_advanced_start/pangram_finder/pangram_finder.js
@@ -1,9 +1,17 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase
 }
 
 PangramFinder.prototype.isPangram = function () {
+  let lower_phrase = this.phrase.toLowerCase()
+  lower_phrase = lower_phrase.split('')
+  const result = this.alphabet.every((letter) => {
+    return lower_phrase.includes(letter)
+  })
+  return result
+} 
 
-}
-
+// const pangramFinder = new PangramFinder('the quick brown fox jumps over the lazy dog');
+// console.log(pangramFinder.isPangram())
 module.exports = PangramFinder;
