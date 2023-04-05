a287542ca6f12eb4fce998554da041520f4241dc
Gergely Farkas <efgeri@gmail.com>
2023-04-05 19:09:27 +0100

    Isogram done

--- week_06/day_3/hw_advanced_start/isogram_finder/isogram_finder.js
+++ week_06/day_3/hw_advanced_start/isogram_finder/isogram_finder.js
@@ -1,9 +1,16 @@
 const IsogramFinder = function (word) {
+    this.word = word.split('')
 
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
+    const result = this.word.every((letter, index) => {
+        return this.word.indexOf(letter) === index
+    })
+    return result
 }
+
+let isIsogram = (str) => str.split("").every((c, i) => str.indexOf(c) == i);
+
 
 module.exports = IsogramFinder;
